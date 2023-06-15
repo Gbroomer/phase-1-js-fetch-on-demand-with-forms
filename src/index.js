170786eaff06a7968890af9469134bf8f4edd716
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-15 14:05:35 -0500

    js fetch completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.getElementById("searchByID");
+    let h4 = document.querySelector("#movieDetails h4");
+    let p = document.querySelector("#movieDetails p")
+    console.log(p)
+    fetch(`http://localhost:3000/movies${input.value}`)
+        .then((resp) => {
+            return resp.json()})
+        .then((data) => {
+            console.log(data);
+            if (data.id) {
+                h4.textContent = data.title
+                p.textContent = summary.title
+            }})
+        .catch((err) => {
+            console.log(err);
+     
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
